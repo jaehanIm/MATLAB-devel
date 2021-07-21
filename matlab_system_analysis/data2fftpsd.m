72305691f8e354e418143b88278a5bf259bf78bb
jaehan <jaehan.im@nearthlab.com>
2021-07-21 10:47:11 +0900

    update 0721

--- matlab_system_analysis/data2fftpsd.m
+++ matlab_system_analysis/data2fftpsd.m
@@ -1,24 +1,26 @@
-function out = data2fftpsd(data)
+function [freq, fftResult, psdResult] = data2fftpsd(data, Fs)
 
 X = data;
 X = X-mean(X);
 L = length(X);
 
-Fs = 50; % sampling rate
-
 Y = fft(X);
 P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:L/2)/L;
-plot(f(1:end),P1(1:end))
-hold on
+fftResult = P2(1:L/2+1);
 
-Pxx = 1/(L*Fs)*abs(Y(1:length(X)/2+1)).^2;
-freq = 0:Fs/L:Fs/2;
-semilogy(freq(2:end),Pxx(2:end));
+freq = Fs*(0:L/2)/L;
+freq = freq';
+fftResult(2:end-1) = 2*fftResult(2:end-1);
+psdResult = 1/(L*Fs)*abs(Y(1:length(X)/2+1)).^2;
 
-grid on
-legend('FFT','PSD')
+
+% figure(5)
+% clf
+% plot(freq(1:end),fftResult(1:end));
+% hold on
+% semilogy(freq(2:end),psdResult(2:end));
+% xlim([0.1 inf])
+% grid on
+% legend('FFT','PSD')
 
 end
