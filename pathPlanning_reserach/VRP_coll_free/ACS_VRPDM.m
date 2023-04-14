cd8ac93afe9b0c9476eac9f8a7063c7f4f9c0a0d
jaehan <jaehan.im@nearthlab.com>
2023-04-14 19:17:24 +0900

    update0414_2

--- pathPlanning_reserach/VRP_coll_free/ACS_VRPDM.m
+++ pathPlanning_reserach/VRP_coll_free/ACS_VRPDM.m
@@ -1,7 +1,7 @@
 
 %% ACOVRP algorithm 
 global homeIdx tau0
-
+stopThres = 200;
 %% Initial parameters of ACO 
 maxIter = 10000;
 
@@ -30,12 +30,14 @@ colony.reservation = [];
 
 for t = 1 : maxIter
     % Create Ants 
-    [colony, incompleteFlag] = createColonyVRPDM( mapGraph, antNo, tau, eta, param, vnum, capacity, servTime, implicitRoute);
-        
+    
+    colony = [];
+    colony = createColonyVRPDM( mapGraph, colony , antNo, tau, eta, param, vnum);
+    
+    
     % Calculate the fitness values of all ants 
     for i = 1 : antNo 
-        [colony.ant(i).fitness, colony.ant(i).fitnessL, colony.ant(i).fitnessPer, colony.ant(i).fitnessM, colony.ant(i).violation] = ...
-            fitnessFunctionVRPCF(colony.ant(i).tour, colony.ant(i).vehTourLen, mapGraph, vnum, colony.ant(i).tickHistory, servTime, param, incompleteFlag, N);
+        [colony.ant(i).fitness, colony.ant(i).fitnessL, colony.ant(i).fitnessPer, colony.ant(i).fitnessM] = fitnessFunctionVRPDM(colony.ant(i).tour, colony.ant(i).vehTourLen, mapGraph, vnum);       
     end
     
     % Find the best ant (queen)
@@ -46,32 +48,20 @@ for t = 1 : maxIter
         bestTour = colony.ant(minIndex).tour;
         bestTourLen = colony.ant(minIndex).vehTourLen;
         bestFitnessL = colony.ant(minIndex).fitnessL;        
-        bestFitnessM = colony.ant(minIndex).fitnessM;
         bestFitnessPer = colony.ant(minIndex).fitnessPer;
-        bestTick = colony.ant(minIndex).tick;
-        bestTickHistory = colony.ant(minIndex).tickHistory;
-        bestViolation = colony.ant(minIndex).violation;
-        bestReservation = colony.ant(minIndex).reservation;
-        bestOccupancy = colony.ant(minIndex).occupancy;
     end
     
     colony.queen.tour = bestTour;
     colony.queen.fitness = bestFitness;
     colony.queen.vehTourLen = bestTourLen;
     colony.queen.fitnessL = bestFitnessL;
-    colony.queen.fitnessM = bestFitnessM;
     colony.queen.fitnessPer = bestFitnessPer;
-    colony.queen.tick = bestTick;
-    colony.queen.tickHistory = bestTickHistory;
-    colony.queen.violation = bestViolation;
-    colony.queen.reservation = bestReservation;
-    colony.queen.occupancy = bestOccupancy;
         
     % Update phromone matrix 
-    tau = updatePhromoneVRPCF(tau , colony, param.rho, minIndex, vnum);
+    tau = updatePhromoneVRPDM(tau , colony, param.rho, minIndex, vnum);  
     
     % Display the results
-    if mod(t,1) == 0
+    if mod(t,50) == 0
         outmsg = [ 'Iteration #' , num2str(t) , ' Shortest length = ' , num2str(colony.queen.fitness)  ];
         disp(outmsg)
     end
@@ -80,33 +70,14 @@ for t = 1 : maxIter
         if colony.queen.fitness == history(t-1)
             count = count + 1;
         else
-            disp('solution update detected!');
+%             disp('solution update detected!');
             count = 0;
         end
     end
 
-    coveredNode = unique(bestTour);
-    coveredNode(1) = [];
-    missingNode = ones(N,1);
-    missingNode(coveredNode) = 0;
-    missingNode = find(missingNode);
-    if ~isempty(missingNode)
-        missingNode
-    end
-
     if count >= stopThres
         disp('Solution stabilized. Terminating ACS!');
-        if ~isempty(missingNode)
-            disp('Solution incomplete');
-            missingNode
-        else
-            disp('Solution complete')
-        end
         break;
     end
-
 end
-VRPCF_solveTime = toc;
-VRPCF_result = colony.queen.fitness;
-VRPCF_resultL = colony.queen.fitnessL;
-VRPCF_resultPerV = colony.queen.fitnessPer;
+soleACO_time = toc;
---
+++ pathPlanning_reserach/VRP_coll_free/fitnessFunctionVRPDM.m
@@ -0,0 +1,33 @@
+function [ fitness, fitnessL, fitnessPerV, fitnessM ] = fitnessFunctionVRPDM (tour, tourLen, graph, vehNum)
+
+
+fitnessPerVeh = zeros(vehNum,1);
+fitnessPerVeh_e = zeros(vehNum,1);
+
+for i = 1:vehNum
+    for j = 1:tourLen(i)
+        if tourLen(i) ~= 1
+            currentNode = tour(i,j);
+            nextNode = tour(i,j+1);
+            fitnessPerVeh(i) = fitnessPerVeh(i) + graph.edges(currentNode, nextNode);
+        else
+            fitnessPerVeh(i) = 0;
+        end
+    end
+end
+
+for i = 1:vehNum
+    for j = 1:tourLen(i)-1
+        currentNode = tour(i,j);
+        nextNode = tour(i,j+1);
+        fitnessPerVeh_e(i) = fitnessPerVeh_e(i) + graph.edges(currentNode, nextNode);
+    end
+end
+
+fitnessM = max(fitnessPerVeh);
+fitnessL = sum(fitnessPerVeh);
+fitnessPerV = fitnessPerVeh;
+fitnessME = max(fitnessPerVeh_e);
+fitness = fitnessM;
+
+end
